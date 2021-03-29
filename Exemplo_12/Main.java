9475fb863e2c64fe590e6dc4cdcba3b3a10eb50e
Herbert Andrade Nascimento <hanascimento.cic@uesc.br>
2021-03-29 06:58:04 +0000

    add ex1

--- Exemplo_12/Main.java
+++ Exemplo_12/Main.java
@@ -5,37 +5,71 @@ import java.util.Scanner;
 //Arquivo para exemplpificar a implementação d e uma classse
 public class Main{
   public static void main(String [] args){
-		Scanner sc = new Scanner(System.in);
     // implementação do corpo da função main
-		System.out.println("Implementando funções");
-		System.out.println("Classse Random: As instâncias desta classe são usadas");
-		System.out.println("para gerar um fluxo de números pseudo-aleatórios");
-		System.out.println("Simulando 10 lançamentos de um dado:");
-		System.out.println("Escolhendo a semente de forma automatica");
-		jogaOsDados(1, 6, 10, 0);
-		System.out.println("Diferente da função rand() de C, esta classe utiliza sementes diferentes a cada execução!");
-		long semente;
-		System.out.println("Entre com a semente para gerar uma nova sequencia: ");
-		semente = sc.nextLong();
-		jogaOsDados(1, 6, 10, semente);
-		System.out.println("Sementes diferentes irão gerar resultados diferentes!");
-		System.out.println("A mesma semente gera o mesmo resultado!");
+    int vetor[], tam;
+		System.out.println("Mega Senna");
+    //--------------------------------//
+    System.out.println("Jogo 1");
+		vetor = GerarVetorNaoRepetido(1, 60, 6, 0);
+    tam = vetor.length;
+    ordenar(vetor,tam);
+    printVet(vetor,tam);
+    //--------------------------------//
+    System.out.println("Jogo 2");
+		vetor = GerarVetorNaoRepetido(1, 60, 6, 0);
+    tam = vetor.length;
+    ordenar(vetor,tam);
+    printVet(vetor,tam);
+    //--------------------------------//
+    System.out.println("Jogo 3");
+		vetor = GerarVetorNaoRepetido(1, 60, 6, 0);
+    tam = vetor.length;
+    ordenar(vetor,tam);
+    printVet(vetor,tam);
+
+
   }
 
-
-	public static void jogaOsDados(int min, int tam, int qtVezes, long semente){
-		Random rand = new Random();
+   public static void ordenar(int vetor[],int tam){
+    int aux;
+    for (int i = 0; i < tam; i++){
+        for (int j = 0; j < tam; j++){
+          if (vetor[i] < vetor[j]){
+              aux = vetor[i];
+              vetor[i] = vetor[j];
+              vetor[j] = aux;
+          }
+      }
+    }
+  }
+  public static int []GerarVetorNaoRepetido(int min, int tam, int qtVezes,int semente){
+   int[ ] vetor = new int[qtVezes];
+    Random rand = new Random();
 		if (semente > 0)
 			rand.setSeed(semente);
-		System.out.print("( ");
-		int dado;
-		for(int i = 1; i <= qtVezes; i++){
-			dado = min + rand.nextInt(tam);
-			System.out.print(dado);
-			if (i < qtVezes)
-				System.out.print(", ");
-			else
-				System.out.println(" )");
-		}
-	}
+    for(int i=0;i<qtVezes;i++){
+      do{
+       vetor[i] = min + rand.nextInt(tam);
+        int rep = 0;
+        for(int j=0;j<i;j++){
+          if(vetor[i]==vetor[j]){
+              rep=1;
+              break;
+          }
+        }
+      }while(false);    
+    }
+    return vetor;
+}
+
+    public static void printVet(int vet[],int tam){
+      System.out.print("( ");
+      for(int i = 0; i < tam; i++){
+        System.out.print(vet[i]);
+        if (i < tam-1)
+          System.out.print(", ");
+        else
+        System.out.println(")");
+	    }
+    }
 }
